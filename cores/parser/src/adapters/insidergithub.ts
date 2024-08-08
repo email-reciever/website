4575e86dde3b701d754fd70e50573abeb5a069fe
innocces <jiangjiangjiangxi@gmail.com>
2024-08-08 10:45:04 +0800

    fix(sitg-plugin): fix sitg error with text-node with url or broken value

--- cores/parser/src/adapters/insidergithub.ts
+++ cores/parser/src/adapters/insidergithub.ts
@@ -1,4 +1,5 @@
 import { Adapter } from '../types';
+import { testURI } from '@email.reciever/consts/regexp';
 
 export const insidergithub: Adapter = (text, html) => {
 	// slice content from the main things to sports
@@ -6,9 +7,7 @@ export const insidergithub: Adapter = (text, html) => {
 	const endIndex = text.indexOf('Try these tips now');
 	const blogContent = text.slice(startIndex, endIndex);
 
-	const origin_url = text
-		.match(/\[https?:\/\/(([a-zA-Z0-9_-])+(\.)?)*(:\d+)?(\/((\.)?(\?)?=?&?[a-zA-Z0-9_-](\?)?)*)*]/i)?.[1]
-		?.slice(1, -1);
+	const origin_url = text.match(testURI)?.[1]?.slice(1, -1);
 
 	return {
 		blogContent,
--- cores/parser/src/remarks/remark-words-sitg-plugin.ts
+++ cores/parser/src/remarks/remark-words-sitg-plugin.ts
@@ -5,13 +5,14 @@ import { type Plugin } from 'unified';
 import type { Node } from 'unist';
 import { visit } from 'unist-util-visit';
 import { type Test } from 'unist-util-is';
+import { testPureURI } from '@email.reciever/consts/regexp';
 
 export const SITGPREFIX = '$';
 
 export const SITGNODETYPE: string[] = ['text'];
 export const ESCAPENODETYPE: string[] = [];
 
-export const escapeTextContent = ['\n', '', '””'];
+export const escapeTextContent = ['\r\n', '\n', '', '””', '"\\"', '""'];
 
 export type CollectSitg = (key: string, value: string) => string;
 
@@ -22,7 +23,12 @@ export interface VisitNode extends Node {
 }
 
 export function shouldSitg(node: VisitNode) {
-	return node.value && !escapeTextContent.includes(node.value);
+	// escape url
+	if (node.value) {
+		// due to some text content may have spaces, use trim value check
+		const trimValue = node.value.trim();
+		return trimValue.length && !testPureURI.test(trimValue) && !escapeTextContent.includes(trimValue);
+	}
 }
 
 let count = 0;
@@ -36,6 +42,7 @@ export const remarkWordsSitgPlugin: Plugin<[CollectSitg]> = (onCollectSitg: Coll
 				// replace node value with sitg mark
 				if (shouldSitg(node)) {
 					const sitgMark = `${SITGPREFIX}${count++}`;
+					console.log(sitgMark, node.value);
 					onCollectSitg(sitgMark, node.value!);
 					node.value = sitgMark;
 					console.log('remarkWordsSitgPlugin', node);
