c83f8239e2259f33a40d8d1af2cc38af09485ced
innocces <jiangjiangjiangxi@gmail.com>
2024-08-15 13:53:52 +0800

    feat(parser): feature createBlog

--- cores/parser/src/index.ts
+++ cores/parser/src/index.ts
@@ -1,6 +1,6 @@
 import postalMime from 'postal-mime';
 import dayjs from 'dayjs';
-import * as process from 'node:process';
+import 'dayjs/locale/zh-cn';
 
 import { WHITELISTS } from '@email.reciever/consts/white-lists';
 import { md } from './adapters/md';
@@ -15,7 +15,13 @@ import { cloudflareblog } from './adapters/cloudflareblog';
 import { arcupdate } from './adapters/arcupdate';
 import { alexkondov } from './adapters/alexkondov';
 import { insidergithub } from './adapters/insidergithub';
-import { Adapter, AdapterOutput, SenderType } from './types';
+
+import { local } from './adapters/local';
+import { github } from './adapters/github';
+
+import { Adapter, SenderType } from './types';
+
+dayjs.locale('zh-cn');
 
 type WlKeys = keyof typeof WHITELISTS;
 
@@ -27,6 +33,58 @@ function getEmailSenderType(email: any) {
 	return Object.keys(WHITELISTS)
 		.find((v) => WHITELISTS[v as WlKeys] === email)!
 		.toLocaleLowerCase() as Lowercase<WlKeys>;
+}
+
+async function createBlog(translateContent: string, originContent: string, frontMatter: frontmatter, env: Env) {
+	const frontMatterContent = Object.entries(frontMatter)
+		.map(([k, v]) => `${k}: ${v}`)
+		.join('\n');
+
+	const content = `---
+${frontMatterContent}
+---
+
+import { Detail } from '@/components/Detail.tsx';
+import { Reference } from '@/components/Reference.tsx';
+
+<Reference client:only="react" title="${frontMatter.title}" url="${frontMatter.origin_url}" />
+
+<Detail client:only="react">
+	${originContent}
+</Detail>
+
+${translateContent}
+
+`;
+
+	const { author } = frontMatter;
+	const fileName = dayjs().format('YYYY-MM-DD_HH-mm-ss_SSS');
+	// astro use filename for slug, some charactor may cause route error, use current-date-timestamp
+	const path = `src/content/${author}/${fileName}.mdx`;
+
+	if (env.ENV === 'dev') {
+		await local({ path, content }, env);
+	} else {
+		await github({ path, content }, env);
+	}
+}
+
+function createCommonFrontmatter(title: string, author: string, origin_url: string, date?: string) {
+	const withFormatDate = dayjs(date).format('dddd hh A MMMM Do YYYY');
+	const avatar = `/media-source/${author}-ico.png`;
+	// deal charactor ':', it will cause astro js-yml error
+	const escapeTitle = title.replace(/:/gi, '_');
+	return {
+		layout: '../../layouts/BlogLayout.astro',
+		title: escapeTitle,
+		description: escapeTitle,
+		date: withFormatDate,
+		author,
+		origin_url,
+		origin_site: origin_url,
+		translated: true,
+		avatar,
+	};
 }
 
 const adapter: Record<SenderType, Adapter> = {
@@ -45,19 +103,30 @@ interface RequestBody {
 	md: string;
 	type: SenderType;
 	html: string;
+	title: string;
+	date: string;
 }
 
 export default {
 	// test route. not avaliable
 	async fetch(request, env) {
-		const { md: markdownContent, type, html } = await request.json<RequestBody>();
-		console.log(import.meta.filename, process.env.PATH);
+		const { md: markdownContent, type, html, title, date } = await request.json<RequestBody>();
 		// debug with https://kreata.ee/postal-mime/example/
 		if (markdownContent?.length) {
 			// with different email sender, use adapter to collect right content for translate
 			const { blogContent = markdownContent, collectLinks, origin_url } = await adapter[type](markdownContent!, html, []);
 			// translate content
 			const translatedMD = await md(blogContent!, env, collectLinks);
+
+			await createBlog(
+				translatedMD,
+				blogContent!,
+				{
+					...createCommonFrontmatter(title, type, origin_url!, date),
+				} as unknown as frontmatter,
+				env
+			);
+
 			return Response.json({ translated: translatedMD, inputs: blogContent, origin_url });
 		}
 
@@ -69,7 +138,6 @@ export default {
 			// collect major infomation
 			const { from, raw } = message;
 			const { text, html, subject, date, headers } = await postalMime.parse(raw);
-			const withFormatDate = dayjs(date).format('YYYY-MM-DD HH:mm:ss');
 
 			// get email sender type, also use for collect dir. example: reactdigest/xxx
 			const dir = getEmailSenderType(from);
@@ -80,21 +148,13 @@ export default {
 			const translatedMD = await md(blogContent!, env, collectLinks);
 
 			const title = subject ?? `${from}`;
-			const avatar = `/media-source/${dir}-ico.png`;
 			// generate frontmatter
 			const basicFrontMatter: frontmatter = {
-				layout: '@/layouts/BlogLayout.astro',
-				title,
-				description: title,
-				date: withFormatDate,
-				avatar,
-				author: dir,
-				origin_url,
-				origin_site: origin_url,
-				translated: true,
+				...createCommonFrontmatter(title, dir, origin_url, date),
 				email_recorder: from,
 			};
-			console.log(__dirname);
+
+			await createBlog(translatedMD, blogContent!, basicFrontMatter, env);
 		} else {
 			message.setReject('Unknown Sender!!');
 		}
