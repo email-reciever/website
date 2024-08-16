935565dc7f1b63f968672ad9bcbb38b056e0cfcb
innocces <jiangjiangjiangxi@gmail.com>
2024-08-16 18:09:47 +0800

    feat(html2md): initial addRules support

--- cores/parser/src/utils/html2md.ts
+++ cores/parser/src/utils/html2md.ts
@@ -2,34 +2,55 @@
  * @description make common rules for transform html to markdown
  */
 import * as cheerio from 'cheerio';
-import { td, type Options, type Node } from './td';
+import { td, type Options, type Rule } from './td';
 
 function cleanAttribute(attribute?: string | null) {
 	return attribute ? attribute.replace(/(\n+\s*)+/g, '\n') : '';
 }
 
-export function html2md(html: string, options?: Partial<Options>) {
+export function html2md(html: string, options?: Partial<Options> & { rules?: [key: string, rule: Rule][] }) {
+	const { rules = [], ...restOptions } = options ?? {};
 	const services = new td({
-		...options,
+		headingStyle: 'atx',
+		codeBlockStyle: 'fenced',
+		...restOptions,
 	});
 
-	// services.addRule('image-sources', {
-	// 	filter: ['img'],
-	// 	// fix: https://github.com/mixmark-io/turndown/blob/cc73387fb707e5fb5e1083e94078d08f38f3abc8/src/commonmark-rules.js#L249
-	// 	replacement: function (content, node) {
-	// 		const imageNode = node as HTMLImageElement;
-	// 		const alt = cleanAttribute(imageNode.getAttribute('alt'));
-	// 		const src = imageNode.getAttribute('src') || '';
-	// 		console.log('image src', src);
-	// 		const title = cleanAttribute(imageNode.getAttribute('title'));
-	// 		const titlePart = title ? ' "' + title + '"' : '';
-	// 		return src ? '![' + alt + ']' + '(' + src + titlePart + ')' : '';
-	// 	},
-	// });
+	rules.forEach((v) => services.addRule(...v));
+
+	services
+		// .addRule('code-block', {
+		// 	filter: ['code'],
+		// 	replacement: function (content, node) {
+		// 		const codeNode = node as HTMLElement;
+		// 		const className = cleanAttribute(codeNode.getAttribute('class')) ?? '';
+		// 		let [, lang = 'text'] = className.split('-');
+		// 		if (!className?.length) {
+		// 			return `\`${content}\``;
+		// 		}
+		// 		// lang maybe error
+		// 		if (/import /.test(content)) {
+		// 			lang = 'tsx';
+		// 		}
+		// 		console.log('code-block', lang, content, '\n```' + lang + '\n' + content + '\n```\n');
+		// 		return '\n```' + lang + '\n' + content + '\n```\n';
+		// 	},
+		// })
+		.addRule('image-sources', {
+			filter: function (node) {
+				const imageNode = node as HTMLImageElement;
+				const src = imageNode.getAttribute('src') || '';
+				return /data:image\/svg\+xml/.test(src);
+			},
+			replacement: function () {
+				return '';
+			},
+		});
 
 	// only use body for translate
 	const bodyHTML = cheerio.load(html, null, false).html();
 	return {
+		bodyHTML,
 		services,
 		md: services.turndown(bodyHTML!),
 	} as const;
