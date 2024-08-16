b35c16e9e3256f48eacfabcacbbdb55a6e307f14
innocces <jiangjiangjiangxi@gmail.com>
2024-08-16 19:25:13 +0800

    fix(build): fix node build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -34,7 +34,7 @@ export default defineConfig({
       }
     },
     ssr: {
-      external: ['node:buffer', 'node:worker_threads']
+      external: ['node:buffer', 'node:worker_threads', 'node:async_hooks']
     },
     build: {
       minify: false,
