c754127f796a65c285cca468d61444ff18cde48c
innocces <jiangjiangjiangxi@gmail.com>
2024-08-28 17:41:03 +0800

    chore(local): instand env TR_SERVE

--- cores/parser/src/adapters/local.ts
+++ cores/parser/src/adapters/local.ts
@@ -3,11 +3,9 @@
  */
 import type { FileInfo } from './github';
 
-const localBaseURI = 'http://localhost:10856';
-
 export async function localRead(fileName: string, env: Env) {
 	try {
-		return await fetch(`${localBaseURI}/read`, {
+		return await fetch(`${env.TR_SERVE}/read`, {
 			method: 'POST',
 			body: JSON.stringify({ fileName }),
 		}).then((response) => response.json());
@@ -20,7 +18,7 @@ export async function localRead(fileName: string, env: Env) {
 
 export async function local(file: FileInfo, env: Env) {
 	try {
-		await fetch(`${localBaseURI}/create`, {
+		await fetch(`${env.TR_SERVE}/create`, {
 			method: 'POST',
 			body: JSON.stringify(file),
 		});
