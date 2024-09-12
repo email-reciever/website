d9550b093d48d0c258cded2cefdf87bf1ed0470b
innocces <jiangjiangjiangxi@gmail.com>
2024-09-12 17:11:59 +0800

    fix(zhihu): remove error log & replace new route

--- packages/consts/src/zhihu.ts
+++ packages/consts/src/zhihu.ts
@@ -1,19 +1,19 @@
 export const ZHROUTE = {
-  posts: '/zhihu/posts/people'
+  posts: '/zhihu/xhu/people/posts'
 }
 
 export const ZHUSER = {
-  wzy66: [
+  e32618a8dec99a99021f7d863f4def4f: [
     '**栗子前端',
     ['**欢迎关注微信公众号：栗子前端**'],
     /栗子前端技术周刊第 (\d+) 期/
   ],
-  'huo-yi-tong-98': [
+  '185e0fcde5a72aaca7ea0c324e5feffb': [
     '大家好，我是童欧巴',
     ['以上就是本期的技术周刊', '你的前端食堂'],
     /^技术周刊/
   ],
-  ftium4: [
+  e85837a1a2e5b73310691883ce3ded31: [
     '系统的知识来源于对碎片的整理和思考',
     ['IP 属地浙江', '信息来源于 新闻报道'],
     /体验碎周报第 (\d+) 期/
